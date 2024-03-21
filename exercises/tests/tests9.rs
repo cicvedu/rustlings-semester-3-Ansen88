b8ad402c954071e9389e035c102b212a6ddf06c4
Ansen <ansen_an@yeah.com>
2024-03-21 14:24:52 +0800

    update

--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -27,16 +27,23 @@
 //
 // You should NOT modify any existing code except for adding two lines of attributes.
 
-// I AM NOT DONE
-
 extern "Rust" {
     fn my_demo_function(a: u32) -> u32;
     fn my_demo_function_alias(a: u32) -> u32;
 }
 
+
 mod Foo {
     // No `extern` equals `extern "Rust"`.
+    #[no_mangle]
+    #[export_name = "my_demo_function"]
     fn my_demo_function(a: u32) -> u32 {
+        a
+    }
+
+    #[no_mangle]
+    #[export_name = "my_demo_function_alias"]
+    fn my_demo_function_alias(a: u32) -> u32{
         a
     }
 }
